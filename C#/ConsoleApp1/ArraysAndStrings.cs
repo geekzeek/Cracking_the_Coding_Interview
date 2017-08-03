0055d2ccbe0793c8b0cb0fde4c7ea0b705698a62
geekzeek <tabalchi222@gmail.com>
2017-08-03 14:22:41 -0700

    Initial commit of C# implementation
    C# implementation of practice questions developed with Visual Studio Community 2017

---
+++ C#/ConsoleApp1/ArraysAndStrings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Cracking the Coding Interview Chapter 1
+class ArraysAndStrings
+{
+    // 1.1 Implement an algorithm to determine if a string has all unique characters.
+    public static bool isUniqueChars(string word)
+    {
+        bool[] charSet = new bool[256];
+        foreach (char letter in word)
+        {
+            if (charSet[(int)letter])
+            {
+                return false;
+            }
+            else charSet[(int)letter] = true;
+        }
+        return true;
+    }
+
+    // 1.2 Write code to reverse a C-Style String.
+    public static string reverse(string word)
+    {
+        int i = 0;
+        string output = "";
+        for (i = word.Length - 1; i >= 0; i--)
+        {
+            output += word[i];
+        }
+        Console.WriteLine(output);
+        return output;
+    }
+
+    // 1.4 Write a method to decide if two strings are anagrams or not.
+    public static bool isAnagram(string word1, string word2)
+    {
+        if (word1.Length != word2.Length) return false;
+        char[] chars1, chars2;
+        chars1 = word1.ToCharArray();
+        chars2 = word2.ToCharArray();
+        Array.Sort(chars1);
+        Array.Sort(chars2);
+        return String.Equals(new string(chars1), new string(chars2));
+    }
+}
---
+++ C#/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ArraysAndStrings.isAnagram("taco", "coat"));
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+  
+}
+
+
