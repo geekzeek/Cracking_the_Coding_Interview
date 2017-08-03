dc529fdbd568c5926920b9a031b0a4e492a1cb55
geekzeek <tabalchi222@gmail.com>
2017-08-03 14:39:01 -0700

    Implementation of question 2.1

--- C#/ConsoleApp1/LinkedLists.cs
+++ C#/ConsoleApp1/LinkedLists.cs
@@ -7,7 +7,23 @@ using System.Threading.Tasks;
 // Cracking the Coding Interview Chapter 2
 class LinkedLists
 {
-    public static void removeDups(LinkedListNode<string> node)
+    // 2.1 Write code to remove duplicates from an unsorted linked list.
+    public static void removeDups(LinkedList<string> list)
     {
+        var node = list.First;
+        HashSet<string> elements = new HashSet<string>();
+        elements.Add(node.Value);
+        while (node.Next != null)
+        {
+            var current = node.Next;
+            if (elements.Contains(current.Value))
+            {
+                list.Remove(current);
+            }
+            else
+            {
+                elements.Add(current.Value);
+            }
+        }
     }
 }
